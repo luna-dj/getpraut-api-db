1d035230cfd7f6dd2770a3fccad923331a0bfb38
Luna <luna@linkse.eu>
2024-11-04 00:21:07 +0100

    make it clear if it's running in developemnt '

---
+++ src/components/DevelopmentBanner.svelte
@@ -0,0 +1,13 @@
+<script>
+    import { onMount } from 'svelte';
+  
+    let isDevelopment = true;
+    const idDev = import.meta.env.DEV;
+    onMount(() => {
+      isDevelopment = import.meta.env.ASTRO_ENV === 'development';
+      console.log(isDevelopment);
+    });
+  </script>
+{#if  !idDev === false}
+<div class="text-center font-bold text-lg text-red-500 bg-red-900 outline mb-4 outline-4 outline-red-500 text-yellow-400">DEVELOPMENT</div>
+{/if}
