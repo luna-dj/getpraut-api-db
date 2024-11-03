49dacb5a30b164e231376edfc340b8cfe84f9a7e
Luna <luna@linkse.eu>
2024-11-03 16:47:23 +0100

    FIX: Errors

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,17 @@
 import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
+import node from '@astrojs/node';
 export default defineConfig({
   // ... other options ...
   output: 'server',
+
   typeScript: {
     enable: true,
   },
 
   integrations: [svelte()],
+
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
